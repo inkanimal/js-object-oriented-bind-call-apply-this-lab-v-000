ce14fbfe27a5cc0b11be4901ab379ca3d2ca70de
geoffrey woodcock <theatomproject@gmail.com>
2019-05-28 19:42:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 function justInvoke(fn){
   return fn();
 }
+
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg);
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(fn, thisValue){
+  return fn.bind(thisValue);
+}
